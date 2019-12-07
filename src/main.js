1ad8836484ea69ae5b0d6eafc49f64fb64954267
xfggh <942590550@qq.com>
2019-12-07 22:05:18 +0800

    引入 vant

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ Vue.config.productionTip = false
 // 引入路由
 import router from './router/index';
 
+// 引入 vant-ui
+import {
+  Button
+} from 'vant'
+Vue.use(Button);
+
 new Vue({
   render: h => h(App),
   router,
